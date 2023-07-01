ceaa7ba6d4151d97f5f60a384d27df6729c0b95a
Eunbin Mun <122220139+eunbinmoon@users.noreply.github.com>
2023-07-01 23:59:38 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let result = 2
+    for(let i=0; i<n; i++){
+        if(i*i === n){
+          result = 1
+            break
+        }
+    }
+    return result
+}
+   
+
+ 
